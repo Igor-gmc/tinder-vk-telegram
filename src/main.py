c8ed3e274dabea77d95653d41411f7ff3e527e9b
Igor-gmc <galackov@gmail.com>
2026-02-16 15:02:16 +0400

    Исправил log_setup.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from src.core.log_setup import setup_logger, get_logger
+from src.core.log_setup import setup_logger
+from src.core.log_setup import get_logger
 from src.core.init_data_dirs import setup_data_dirs
 from src.core.config import LoggingConfig
 
@@ -6,12 +7,12 @@ def main():
     loger_cfg = LoggingConfig()
 
     # Если папки для обработки данных и логов не созданы, то создаем их
-    setup_data_dirs(loger_cfg)
+    setup_data_dirs(loger_cfg)  # используется только в main.py
+    setup_logger(loger_cfg)     # используется только в main.py
 
-    setup_logger(loger_cfg)
-    log = get_logger(__name__)
-
-    log.info('Старт проекта')
+    # получаем логгер для текущего файла.py
+    log = get_logger(__name__)  # ничего менять не надо
+    log.info('Старт проекта')   # логирование вспомогательной информации или статуса и подобное
 
 if __name__ == '__main__':
     main()
