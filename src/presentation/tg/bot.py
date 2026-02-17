b06f54fb9d7b91d8150904e29b64b75685fd8653
Igor-gmc <galackov@gmail.com>
2026-02-17 16:26:38 +0400

    Создано авторизация и настройка фильтров для поиска VK

--- src/presentation/tg/bot.py
+++ src/presentation/tg/bot.py
@@ -1,1 +1,38 @@
 # Bot/Dispatcher, регистрация роутеров
+
+from src.infrastructure.db.repositories import InMemoryUserRepo
+from src.presentation.tg.handlers import setup_handlers
+from src.core.config import TG_TOKEN
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
+
+def setup_bot(token: str) -> tuple[Dispatcher, Bot]:
+    """
+    Собирает бота и диспетчер так, чтобы можно было тестить другим token.
+    Возвращает (dp, bot).
+    """
+    # FSM память
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+
+    # routers
+    user_repo = InMemoryUserRepo()
+    router = setup_handlers(user_repo)
+    dp.include_router(router=router)
+
+    bot = Bot(token=token, default=DefaultBotProperties(parse_mode="HTML"))
+    return dp, bot
+
+
+async def start_bot() -> None:
+    # получаем Dispatcher, Bot
+    dp, bot = setup_bot(token=TG_TOKEN)
+    
+    # запуск бота    
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close() # закрытие сессии бота
+
